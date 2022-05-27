985014d3379ab52a7e699afeb3cc516ef9841155
DAASHeo <hibstar719@naver.com>
2022-05-27 17:35:03 +0900

    0527

---
+++ a1.js
@@ -0,0 +1,6 @@
+var heading = document.querySelector('#head1')
+        heading.onclick=function()
+        {
+            heading.style.color="white";
+            heading.style.background="orange";
+        }
